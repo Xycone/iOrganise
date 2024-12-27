3c1e3c40fcc5b72f3bd95a0b5a4a9ada8c1ad124
Sean <seanang123456@gmail.com>
2024-12-27 17:23:36 +0800

    Updated llm settings

--- iOrganiseAPI/manager/llamacppManager.py
+++ iOrganiseAPI/manager/llamacppManager.py
@@ -22,7 +22,7 @@ class LlamaCppManager:
             }
 
             if device == "cuda":
-                llm_params["n_gpu_layers"] = 30
+                llm_params["n_gpu_layers"] = -1
                 llm_params["n_batch"] = 512
             
             self.__llm = LlamaCpp(**llm_params)

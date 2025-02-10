51dea47e478d0cd10a1434cdb46920b101f0a35f
Sean <seanang123456@gmail.com>
2025-02-11 07:32:44 +0800

    Updated Login UI

--- iOrganiseUI/src/pages/Login.jsx
+++ iOrganiseUI/src/pages/Login.jsx
@@ -1,102 +1,127 @@
-import React, { useState } from 'react';
-import {
-    Box,
-    Typography,
-    Grid,
-    Divider,
-    Button,
-    Select,
-    Checkbox,
-    MenuItem,
-    FormControl,
-    Dialog,
-    DialogContent,
-    DialogContentText,
-    IconButton,
-    useTheme,
-    Link,
-    TextField,
-} from '@mui/material';  // Make sure Link is imported from 'react-router-dom'
-import Header from '../components/Header';
-import { Link as RouterLink } from 'react-router-dom';
-import GoogleIcon from '@mui/icons-material/Google';
+import React from 'react';
+import { useNavigate, Link } from 'react-router-dom';
+
+import { Box, Typography, Button, TextField, useTheme } from '@mui/material';
 import { tokens } from '../themes/MyTheme';
 
+// Used for backend API call
+import http from '../http';
+
+// Form & Form Validation
+import * as yup from 'yup';
+import { useFormik } from 'formik';
+
+// React Components
+import Header from '../components/Header';
+
+// Toast
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 function Login() {
+    const navigate = useNavigate();
+
     const theme = useTheme();
     const colours = tokens(theme.palette.mode);
 
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+    const formik = useFormik({
+        // Default Form Values
+        initialValues: {
+            username: "",
+            password: ""
+        },
 
-    const handleEmailChange = (e) => setEmail(e.target.value);
-    const handlePasswordChange = (e) => setPassword(e.target.value);
+        // Validation Schema
+        validationSchema: yup.object({
+            username: yup.string().trim()
+                .email('Enter a valid email')
+                .min(1, 'Password must be at least 1 character')
+                .max(255, 'Email must be at most 255 characters')
+                .required('Email is required'),
+            password: yup.string().trim()
+                .min(1, 'Password must be at least 1 character')
+                .max(255, 'Password must be at most 255 characters')
+                .required('Password is required')
+        }),
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        // Handle form submission here (e.g., validate credentials)
-        console.log('Email:', email);
-        console.log('Password:', password);
-    };
+        onSubmit: (data) => {
+            const formData = new FormData();
 
-    const handleGoogleLogin = () => {
-        // Handle Google sign-in here (could use Firebase or other services)
-        console.log('Sign in with Google');
-    };
+            formData.append('username', data.username.trim().toLowerCase());
+            formData.append('password', data.password.trim());
+
+            http.post("/login", formData)
+                .then((res) => {
+                    localStorage.setItem("accessToken", res.access_token);
+                    navigate("/home")
+                })
+                .catch(function (err) {
+                    const errorMessage = err.response?.data?.detail || err.message || 'An error occurred';
+                    toast.error(errorMessage);
+                });
+        }
+    });
 
     return (
-        <Box
-            display="flex"
-            flexDirection="column"
-            alignItems="center"
-            justifyContent="center"
-            sx={{ width: '100%', maxWidth: 400, margin: 'auto', padding: 3 }}
-        >
-            <Header title="Login" />
-            {/* Email Input */}
-            <TextField
-                label="Email"
-                type="email"
-                fullWidth
-                value={email}
-                onChange={handleEmailChange}
-                size='small'
-                sx={{ mb: 1, borderRadius: 5 }}
-            />
+        <Box px={5} pb={5}>
+            <Box
+                display="flex"
+                flexDirection="column"
+                justifyContent="center"
+                alignItems="center"
+            >
+                <Header title="Login" />
 
-            {/* Password Input */}
-            <TextField
-                label="Password"
-                type="password"
-                fullWidth
-                value={password}
-                onChange={handlePasswordChange}
-                size='small'
-                sx={{ mb: 2, borderRadius: 5 }}
-            />
-            <Typography>
-                Don't have an account?{' '}
-                <Link
-                    component={RouterLink}
-                    to="/register"
-                    underline="hover"
-                    sx={{ color: colours.greenAccent[500] }}
-                >
-                    Register here
-                </Link>
-            </Typography>
+                <Box component="form">
+                    <TextField
+                        sx={{ my: 1 }}
+                        fullWidth
+                        size="small"
+                        label="Email"
+                        name="username"
+                        value={formik.values.username}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        error={formik.touched.username && Boolean(formik.errors.username)}
+                        helperText={formik.touched.username && formik.errors.username}
+                    />
 
-            {/* Submit Button (Smaller Size) */}
-            <Box mt={2}>
-                <Button
-                    variant="contained"
-                    color="primary"
-                    onClick={handleSubmit}
-                >
-                    Login
-                </Button>
+                    <TextField
+                        sx={{ my: 1 }}
+                        fullWidth
+                        size="small"
+                        label="Password"
+                        name="password"
+                        type="password"
+                        value={formik.values.password}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        error={formik.touched.password && Boolean(formik.errors.password)}
+                        helperText={formik.touched.password && formik.errors.password}
+                    />
+
+                    <Box my={3}>
+                        <Button
+                            fullWidth
+                            type="submit"
+                            component="label"
+                            size="large"
+                            onClick={formik.handleSubmit}
+                        >
+                            Sign In
+                        </Button>
+                    </Box>
+                    <ToastContainer />
+
+                    <Typography
+                        my={1}
+                        textAlign="center"
+                    >
+                        New user? <Link to="/register" style={{ color: colours.greenAccent[300] }}>Create an account.</Link>
+                    </Typography>
+                </Box>
             </Box>
-        </Box >
+        </Box>
     );
 }
 

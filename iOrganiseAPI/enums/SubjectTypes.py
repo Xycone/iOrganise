55c1d2f5f795bbdccefd00296aa0a4378eefaa3b
Sean <seanang123456@gmail.com>
2025-02-17 00:54:22 +0800

    added subject

---
+++ iOrganiseAPI/enums/SubjectTypes.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+class SubjectTypes(str, Enum):
+    english = "english"
+    math = "math"
+    science = "science"    
--- iOrganiseAPI/model/FileUpload.py
+++ iOrganiseAPI/model/FileUpload.py
@@ -1,6 +1,8 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, Enum as SqlEnum
 from sqlalchemy.orm import relationship
 from database import Base
+
+from enums.SubjectTypes import SubjectTypes
 
 class FileUpload(Base):
     __tablename__ = "file_uploads"
@@ -10,6 +12,7 @@ class FileUpload(Base):
     type = Column(String(20), nullable=False)
     size = Column(Integer, nullable=False)
     path = Column(String(255), unique=True, index=True, nullable=False)
+    subject = Column(SqlEnum(SubjectTypes), nullable=False)
     content_path = Column(String(255), unique=True, index=True, nullable=True)
     summary_path = Column(String(255), unique=True, index=True, nullable=True)
     user_id = Column(Integer, ForeignKey('users.id'))

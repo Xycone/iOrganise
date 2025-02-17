d5040c2549958a978cbeb5e957b7e55aa6913920
Sean <seanang123456@gmail.com>
2025-02-17 08:30:04 +0800

    reduce max tokens

--- iOrganiseAPI/manager/llamacppManager.py
+++ iOrganiseAPI/manager/llamacppManager.py
@@ -19,7 +19,7 @@ class LlamaCppManager:
             llm_params = {
                 "model_path": model_path,
                 "temperature": 0.4,
-                "max_tokens": 2048,
+                "max_tokens": 1024,
                 "n_ctx": context_length
             }
 
